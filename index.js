afe3cbff67958ce7a8216bc46407854efc284151
JTSwisher <jefftswisher@gmail.com>
2020-05-14 11:17:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+//
+
+const random = Math.random();
+
+//
+
+const mod = 16 % 6;
+
+//
+
+const max = Math.max(1, 2, 4, 20);
