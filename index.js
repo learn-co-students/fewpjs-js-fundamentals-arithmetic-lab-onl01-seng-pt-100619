77c5e0be071417ecb2ff8c2140baad14866d5842
emanningt <emanningmusic@yahoo.com>
2020-06-03 14:13:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2 
+
+const random = Math.random()
+
+
+const mod = 4 % 20
+
+const max = Math.max(1, 2, 20, 5, 7, 12);
